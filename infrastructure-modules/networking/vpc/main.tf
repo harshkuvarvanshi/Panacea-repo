95b624e2939436a920cfbf1d51e4218095e421ed
nikhil <nikhilsirvi9@gmail.com>
2026-03-27 10:59:29 +0530

    added vpc module

---
+++ infrastructure-modules/networking/vpc/main.tf
@@ -0,0 +1,85 @@
+############################
+# Provider
+############################
+provider "aws" {
+  region = var.aws_region
+}
+
+############################
+# VPC
+############################
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.name}-vpc"
+  }
+}
+
+############################
+# Internet Gateway
+############################
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+############################
+# Public Subnet
+############################
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-subnet"
+  }
+}
+
+############################
+# Private Subnet
+############################
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name = "${var.name}-private-subnet"
+  }
+}
+
+############################
+# Public Route Table
+############################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+}
+
+############################
+# Route to Internet
+############################
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+############################
+# Associate Public Subnet
+############################
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
